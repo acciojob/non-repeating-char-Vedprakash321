cc457dc07e68187c0f3838eda69c9533cc1e0a84
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-12 14:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function firstNonRepeatedChar(str) {
 	
 	for(let i=0;i<n;i++){
 		 let char = str[i];
-	    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+	    if (str.indexOf(char, i + 1) == -1) {
 	      return char;
 	    }
 			
