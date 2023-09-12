aab9921f0e3d515efd7e460a71785c109c85f718
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-12 14:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n=str.length;
+	
+	for(let i=0;i<n;i++){
+		 let char = str[i];
+	    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+	      return char;
+	    }
+			
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
